0d82f63e3820d3e000266c90d0e06f7dacfee635
Miguel J <anonymusacount2704@gmail.com>
2022-11-15 22:05:49 -0500

    Se realizó una factorización de elementos reiterativos con funciones de JS

--- Ingenieria Web Semestral/Clasificacion.php
+++ Ingenieria Web Semestral/Clasificacion.php
@@ -1,36 +1,28 @@
 <!DOCTYPE html>
- <html>
-   <head>
-        <meta charset='utf-8'>
-        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-        <title>Home</title>
-        <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <link rel="stylesheet" href="resources/main.css">
-        <link rel="icon" href="resources/img/Logo.png">
-   </head>
-   <body>
-           <header class="header-home">
-                
-                <img src="resources/img/Logo.png" alt="IconSoloCiencia" >
-                <h1 style="font-family: var(--fuente_Header) ;"><a href="loginsemestral.php" style="text-decoration: none;color: #7f1947;">Iniciar Sesión </a>|<a href="registrarse.html" style="text-decoration: none;color: #7f1947"> Registrarse</a></h1>
-                
-           </header>
-           <nav class="navbar-home">
-                <ul style="font-family: var(--fuente_titulos);">
-                    <li><a href="Equipos.php">Equipos</a></li>
-                    <li><a href="Resultados.php">Resultados</a></li>
-                    <li><a href="TablaPosicion.php">Tabla de posición</a></li>
-                    <li><a href="Clasificacion.php">Clasificación</a></li>
-                    <li><a href="Favorito.php">Favorito</a></li>
-                </ul>
-           </nav>
+<html>
 
-            <section>
+<head>
+     <meta charset='utf-8'>
+     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+     <title>Home</title>
+     <meta name='viewport' content='width=device-width, initial-scale=1'>
+     <link rel="stylesheet" href="resources/main.css">
+     <link rel="icon" href="resources/img/Logo.png">
+     <script src="./resources/js/templates.js"></script>
+</head>
+
+<body>
+     <div id="general_header"></div>
+
+     <section>
 
 
-        
-                
-            </section>
 
-    </body>
- </html>
+
+     </section>
+     <script>
+          headerTemplate()
+     </script>
+</body>
+
+</html>
--- Ingenieria Web Semestral/Home.php
+++ Ingenieria Web Semestral/Home.php
@@ -8,27 +8,13 @@
      <meta name='viewport' content='width=device-width, initial-scale=1'>
      <link rel="stylesheet" href="resources/main.css">
      <link rel="icon" href="resources/img/Logo.png">
+     <script src="./resources/js/templates.js"></script>
+     
 </head>
 
 <body>
-     <header class="header-home">
-
-          <img src="resources/img/Logo.png" alt="IconSoloCiencia">
-          <h1 style="font-family: var(--fuente_Header) ;">
-               <a href="loginsemestral.php" style="text-decoration: none;color: #7f1947;">Iniciar Sesión </a>|
-               <a href="registrarse.php" style="text-decoration: none;color: #7f1947">Registrarse</a>
-          </h1>
-
-     </header>
-     <nav class="navbar-home">
-          <ul style="font-family: var(--fuente_titulos);">
-               <li><a href="Equipos.php">Equipos</a></li>
-               <li><a href="Resultados.php">Resultados</a></li>
-               <li><a href="TablaPosicion.php">Tabla de posición</a></li>
-               <li><a href="Clasificacion.php">Clasificación</a></li>
-               <li><a href="Favorito.php">Favorito</a></li>
-          </ul>
-     </nav>
+     
+     <div id="general_header"></div>
 
      <section class="sechome">
 
@@ -60,10 +46,12 @@
                </div>
 
           </div>
-
-
      </section>
 
+
+     <script>
+          headerTemplate()
+     </script>
 </body>
 
 </html>
