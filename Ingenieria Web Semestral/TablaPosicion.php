235c3a4dabdc8989f33ce7096b5621324b5db360
Miguel J <anonymusacount2704@gmail.com>
2022-11-15 20:19:10 -0500

    Se normalizó el uso de archivos php, las carpetas de rescursos y arregló las referencias de todos los archivos para amoldarse a los datos

---
+++ Ingenieria Web Semestral/TablaPosicion.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+     <meta charset='utf-8'>
+     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+     <title>Home</title>
+     <meta name='viewport' content='width=device-width, initial-scale=1'>
+     <link rel="stylesheet" href="resources/main.css">
+     <link rel="icon" href="resources/img/Logo.png">
+</head>
+
+<body>
+     <header class="header-home">
+
+          <img src="resources/img/Logo.png" alt="IconSoloCiencia">
+          <h1 style="font-family: var(--fuente_Header) ;"><a href="loginsemestral.php" style="text-decoration: none;color: #7f1947;">Iniciar Sesión </a>|<a href="registrarse.html" style="text-decoration: none;color: #7f1947"> Registrarse</a></h1>
+
+     </header>
+     <nav class="navbar-home">
+          <ul style="font-family: var(--fuente_titulos);">
+               <li><a href="Equipos.php">Equipos</a></li>
+               <li><a href="Resultados.php">Resultados</a></li>
+               <li><a href="TablaPosicion.php">Tabla de posición</a></li>
+               <li><a href="Clasificacion.php">Clasificación</a></li>
+               <li><a href="Favorito.php">Favorito</a></li>
+          </ul>
+     </nav>
+
+     <section>
+
+
+
+
+     </section>
+
+</body>
+
+</html>
