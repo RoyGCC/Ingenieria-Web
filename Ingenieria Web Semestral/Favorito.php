788753bc8e7914c1650852d99beb3421bb48c855
Miguel J <anonymusacount2704@gmail.com>
2022-11-28 23:04:30 -0500

    No Commit No Life Se añadio las caracteristicas restantes de la pestaña de favorito

--- Ingenieria Web Semestral/Favorito.php
+++ Ingenieria Web Semestral/Favorito.php
@@ -1,12 +1,16 @@
 <?php
+include('phpscripts.php');
+include('config.php');
 session_start();
 if (!empty($_SESSION['user'])) {
      echo $_SESSION['user'];
      echo $_SESSION['myrank'];
 }
+else{
+     header('Location:Equipos.php');
+}
 ?>
 <html>
-
 <head>
      <meta charset='utf-8'>
      <meta http-equiv='X-UA-Compatible' content='IE=edge'>
@@ -17,55 +21,66 @@ if (!empty($_SESSION['user'])) {
      <script src="./resources/js/templates.js"></script>
 
      <script lang="javascript" type="text/javascript">
-
-        function seleccionarEquipo(equipo) {
-            location.href = "equipo.php?equipoSeleccionado=" + equipo;
-        }
-
-    </script>
-
+          function seleccionarEquipo(equipo) {
+               location.href = "equipo.php?equipoSeleccionado=" + equipo;
+          }
+     </script>
 </head>
-
 <body class="general_backgroundImage">
      <div id="general_header"></div>
-     
-
-
      <section class="secequipos">
-
-
           <?php
-          include('config.php');
-               $sql_query = "
+          $sql_query = "
                SELECT *
                FROM usuarios u join equipos e
                ON e.id = u.id_equipo_fav
-               WHERE u.id =". $_SESSION['user'] ."
-               ;";  
-               if ($conn_bd) {
-                    $favorite_result = mysqli_query($conn_bd, $sql_query);
-                    while ($equipos = mysqli_fetch_assoc($favorite_result)) {
-                         echo "
+               WHERE u.id =" . $_SESSION['user'] . "
+               ;";
+          if ($conn_bd) {
+               $favorite_result = mysqli_query($conn_bd, $sql_query);
+               
+               while ($equipos = mysqli_fetch_assoc($favorite_result)) {
+                    $fav_team_p = $equipos['id'];
+                    echo "
                          <div class='equipo'>
-                              <a class='equipocontainer' href='equipo.php?equipoSeleccionado=". $equipos['id'] ."'>
+                              <a class='equipocontainer' href='equipo.php?equipoSeleccionado=" . $equipos['id'] . "'>
                               <span>" . $equipos['grupo'] . "</span>
                               <img class='equipoimg' src='./resources/img/Banderas/" . $equipos['dir_bandera'] . "'>
                               <span>" . $equipos['equipo'] . "</span>
                               </a>
                               </div>";
-                    }  
                }
-
+          }
           ?>
-
      </section>
-
-          <script>
-               <?php
-               include 'phpscripts.php';
-               chooseheader(); 
-               ?>
-          </script>
+     <section class="sec_partido">
+          <h1>PARTIDOS POR JUGAR O EN JUEGO</h1>
+          <?php
+          $fav_team = $GLOBALS['fav_team_p'];
+          $sql_all_matches = "SELECT * 
+               FROM partidos p LEFT JOIN equipos e 
+               ON  p.id_equipo1 = e.id && p.id_equipo2 = e.id && p.id_equipo_ganador = e.id
+               WHERE p.estado != 'Finalizado' AND (p.id_equipo1 = '".$fav_team."' OR p.id_equipo2 = '".$fav_team."')
+               ORDER BY p.horario_juego ASC;";
+          display_matches($sql_all_matches);
+          ?>
+     </section>
+     <section class="sec_partido">
+          <h1>PARTIDOS Finalizados</h1>
+          <?php
+          $fav_team = $GLOBALS['fav_team_p'];
+          $sql_query_played = "SELECT * 
+               FROM partidos p LEFT JOIN equipos e 
+               ON  p.id_equipo1 = e.id && p.id_equipo2 = e.id && p.id_equipo_ganador = e.id
+               WHERE p.estado = 'Finalizado' AND (p.id_equipo1 = '".$fav_team."' OR p.id_equipo2 = '".$fav_team."')
+               ORDER BY p.horario_juego ASC;";
+          display_matches($sql_query_played);
+          ?>
+     </section>
+     <script>
+          <?php
+          chooseheader();
+          ?>
+     </script>
 </body>
-
 </html>
