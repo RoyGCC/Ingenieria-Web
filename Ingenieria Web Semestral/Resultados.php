503d161794b9d0f8e2959b1bfa7276397fc594ee
Miguel J <anonymusacount2704@gmail.com>
2022-11-28 21:06:23 -0500

    Arreglo de página de Resultados

--- Ingenieria Web Semestral/Resultados.php
+++ Ingenieria Web Semestral/Resultados.php
@@ -1,4 +1,5 @@
 <?php
+include('phpscripts.php');
 session_start();
 if (!empty($_SESSION['user'])) {
      echo $_SESSION['user'];
@@ -6,7 +7,6 @@ if (!empty($_SESSION['user'])) {
 }
 ?>
 <html>
-
 <head>
      <meta charset='utf-8'>
      <meta http-equiv='X-UA-Compatible' content='IE=edge'>
@@ -16,20 +16,34 @@ if (!empty($_SESSION['user'])) {
      <link rel="icon" href="resources/img/Logo.png">
      <script src="./resources/js/templates.js"></script>
 </head>
-
 <body class="general_backgroundImage">
      <div id="general_header"></div>
-
-     <section>
-
-
+     <section class="sec_partido">
+          <h1>PARTIDOS POR JUGAR O EN JUEGO</h1>
+          <?php
+          $sql_all_matches = "SELECT * 
+                                   FROM partidos p LEFT JOIN equipos e 
+                                   ON  p.id_equipo1 = e.id && p.id_equipo2 = e.id && p.id_equipo_ganador = e.id
+                                    WHERE p.estado != 'Finalizado'
+                                   ORDER BY p.horario_juego ASC;";
+          display_matches($sql_all_matches);
+          ?>
+     </section>
+     <section class="sec_partido">
+          <h1>PARTIDOS Finalizados</h1>
+          <?php
+          $sql_query_played = "SELECT * 
+                              FROM partidos p LEFT JOIN equipos e 
+                              ON  p.id_equipo1 = e.id && p.id_equipo2 = e.id && p.id_equipo_ganador = e.id
+                              WHERE p.estado = 'Finalizado'
+                              ORDER BY p.horario_juego ASC;";
+          display_matches($sql_query_played);
+          ?>
      </section>
      <script>
           <?php
-          include 'phpscripts.php';
           chooseheader();
           ?>
      </script>
 </body>
-
 </html>
